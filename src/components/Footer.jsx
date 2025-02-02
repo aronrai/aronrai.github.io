114c99024996774a09e7d4f7b65496ea405581e0
Aron Rai <aron.dumi.rai@example.com>
2025-02-02 16:30:50 +0530

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,9 @@ import { FaHeart } from "react-icons/fa";
 const Footer = () => {
   return (
     <footer>
-      <p style={{textAlign: 'center', padding: '50px 0px 20px 0px'}}>
-        Made with <span><FaHeart style={{fontSize: '0.7rem', color: 'white'}} /></span> by Aron Rai.</p>
+      <p style={{textAlign: 'center', padding: '100px 0px 20px 0px'}}>
+        Made with <span><FaHeart style={{fontSize: '0.7rem', color: 'white'}} /></span> by Aron Rai.
+      </p>
     </footer>
   )
 }
