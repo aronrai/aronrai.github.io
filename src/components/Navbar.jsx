596752df925f4a457fa54c2a68e5e085dfbc00e0
Aron Rai <aron.dumi.rai@example.com>
2025-02-02 16:28:22 +0530

    font size update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,49 +1,3 @@
-// import { useState, useRef, useEffect } from "react";
-// import { FiMenu } from "react-icons/fi";
-// import { Link } from "react-router-dom";
-
-// function Navbar() {
-//   const [isOpen, setIsOpen] = useState(false);
-//   const navRef = useRef(null);
-
-//   const handleClickOutside = (event) => {
-//     if (navRef.current && !navRef.current.contains(event.target)) {
-//       setIsOpen(false);
-//     }
-//   };
-
-//   useEffect(() => {
-//     if (isOpen) {
-//       document.addEventListener("mousedown", handleClickOutside);
-//     } else {
-//       document.removeEventListener("mousedown", handleClickOutside);
-//     }
-
-//     return () => {
-//       document.removeEventListener("mousedown", handleClickOutside);
-//     };
-//   }, [isOpen]);
-
-//   return (
-//     <nav className="navbar" ref={navRef}>
-//       <div className="nav-container">
-//         <h1 className="logo">Aron Rai</h1>
-//         <div className={`nav-links ${isOpen ? "active" : ""}`}>
-//           <Link to="/">Home</Link>
-//           <Link to="/about">About</Link>
-//           <Link to="/works">Works</Link>
-//           <Link to="/contact">Contact</Link>
-//         </div>
-//         <button className="nav-toggle" onClick={() => setIsOpen(!isOpen)}>
-//           <FiMenu />
-//         </button>
-//       </div>
-//     </nav>
-//   );
-// }
-
-// export default Navbar;
-
 import { useState, useRef, useEffect } from "react";
 import { FiMenu } from "react-icons/fi";
 import { Link } from "react-router-dom";
@@ -52,28 +6,22 @@ function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   const navRef = useRef(null);
 
-  // Function to handle click outside of the navbar (to close the menu)
   const handleClickOutside = (event) => {
     if (navRef.current && !navRef.current.contains(event.target)) {
-      setIsOpen(false); // Close the menu when clicking outside
+      setIsOpen(false);
     }
   };
 
   useEffect(() => {
-    // Add or remove the event listener based on the menu state
     if (isOpen) {
       document.addEventListener("mousedown", handleClickOutside);
     } else {
       document.removeEventListener("mousedown", handleClickOutside);
     }
-
-    // Cleanup the event listener on component unmount
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [isOpen]);
-
-  // Close the sidebar when a link is clicked
   const handleLinkClick = () => {
     setIsOpen(false);
   };
@@ -89,7 +37,7 @@ function Navbar() {
           <Link to="/contact" onClick={handleLinkClick}>Contact</Link>
         </div>
         <button className="nav-toggle" onClick={() => setIsOpen(!isOpen)}>
-          <FiMenu />
+          <FiMenu size={30} />
         </button>
       </div>
     </nav>
